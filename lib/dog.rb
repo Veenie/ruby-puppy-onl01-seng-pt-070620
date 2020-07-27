5d935aa9632cfb3bbb99a8bde5954e18f6c3ca60
Raveen K Sharma <raveen08@gmail.com>
2020-07-27 18:54:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Add your code here
+
+class Dog
+  @@all = [] 
+  
+  def initialize
+    save
+  end
+  
+  def save
+    @@all << self
+  
+  def all
+    @@all
+  end
+  
+  def print_all
+    @@all.each do |dog|
+      puts dog
+    end
+  end  
