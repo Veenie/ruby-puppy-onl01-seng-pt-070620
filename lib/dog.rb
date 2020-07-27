8aee86c0affa35f73113feb180c573cbabe9a22b
Raveen K Sharma <raveen08@gmail.com>
2020-07-27 18:57:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,11 +12,11 @@ class Dog
     @@all << self
   end  
   
-  def all
+  def self.all
     @@all
   end
   
-  def print_all
+  def self.print_all
     @@all.each do |dog|
       puts dog
     end
